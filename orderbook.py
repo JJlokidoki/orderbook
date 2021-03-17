455987bcf0eb54edb5a5797c5f24b3cde9fe87c9
Your Name <you@example.com>
2021-03-17 13:26:20 +0300

    half tests is done

--- orderbook.py
+++ orderbook.py
@@ -12,20 +12,29 @@ class UndefinedID(Exception):
         super().__init__(self.msg)
 
 
+class UndefinedType(Exception):
+    """
+    Custom exception for unknown transaction type
+    """
+    def __init__(self, t_type):
+        self.msg = f"{t_type} unknown transaction type. Use 'bid' or 'ask' "
+        super().__init__(self.msg)
+
+
 class Order(NamedTuple):
     """ 
-    Набор параметров заявки:
+    Order params:
     1. type (bid/ask) 
     2. price
     3. volume
     4. placement time
-    5. action (0 - come, 1 - delete)
+    5. status (0 - come, 1 - delete)
     """
     trans_type: str
     price: float
     volume: int
     placement_time: str
-    action: int
+    status: int
 
     def __str__(self):
         return f"Transaction type is: {self.trans_type}; Price: {self.price}; Volume of order: {self.volume}"
@@ -38,34 +47,44 @@ class OrderBook:
     2. order - place for preservation
     3. date 
     """
-    def __init__(self, item):
+    def __init__(self, item: str):
         self.item = item
         self.order = {}
         self.order_id = 0
         self.date = datetime.now().strftime("%d/%m/%Y")
+        self.deleted_orders = []
 
-    def create_order(self, trans_type, price, volume):
+    def create_order(self, trans_type: str, price: float, volume: int):
+        """
+        Create new order in orders book
+        """
+        if trans_type != 'bid' or trans_type != 'ask':
+            raise UndefinedType(trans_type)
         new_id = 1
         place_time = datetime.now().strftime("%H:%M:%S")
         self.order.update({new_id: Order(trans_type, price, volume, place_time, 0)})
 
-    def remove_order(self, id_to_del):
+    def remove_order(self, id_to_del: int):
+        """
+        Change status of order to 'delete'
+        """
         if id_to_del in self.order:
-            self.order.update({id_to_del: self.order[id_to_del]._replace(action = 1)})
+            self.deleted_orders.append({id_to_del: self.order[id_to_del]._replace(status = 1)})
+            self.order.pop(id_to_del)
             return 1
         raise UndefinedID(id_to_del)
 
-    def get_order_data(self, id_for_get):
-        if id_to_del in self.order:
-            return self.order[id_for_get]
+    def get_order_data(self, id_for_get: int):
+        """
+        Getting data of order
+        """
+        if id_for_get in self.order:
+            return str(self.order[id_for_get])
         raise UndefinedID(id_for_get)
 
     def snapshot(self):
         """
-        template = {"asks": [{
-            "price": 123,
-            "volume": 1000
-        }]
+        Getting snapshot of order book
         """
         tmp_dict = self._sort_orders()
         bids_list = []
@@ -84,12 +103,11 @@ class OrderBook:
                                       "quantity": 0})
                 else:
                     asks_list[-1].update({"quantity": asks_list[-1]["quantity"] + o.volume})
-        return  {"asks": asks_list, "bids": bids_list}
+        return {"asks": asks_list, "bids": bids_list}
 
 
     def _sort_orders(self):
         return OrderedDict(sorted(self.order.items(), key = lambda t: t[1].price))
-
 
 
 if __name__ == "__main__":
